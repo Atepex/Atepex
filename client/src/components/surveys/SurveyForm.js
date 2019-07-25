87749b83f12ab697cfd690ac676970c2a94a63fc
Emery Haddy <e_haddy@hotmail.com>
2019-07-24 21:29:16 -0700

    users can see survey results

---
+++ client/src/components/surveys/SurveyForm.js
@@ -0,0 +1,52 @@
+import _ from 'lodash';
+import React, { Component } from 'react';
+import { reduxForm, Field } from 'redux-form';
+import { Link } from 'react-router-dom';
+import SurveyField from './SurveyField';
+import validateEmails from '../../utilities/validateEmails';
+import formFields from './formFields';
+
+class SurveyForm extends Component {
+	renderFields() {
+		return _.map(formFields, ({ name, label }) => {
+			return <Field key={name} component={SurveyField} type="text" name={name} label={label} />;
+		});
+	}
+
+	render() {
+		return (
+			<div>
+				<form onSubmit={this.props.handleSubmit(this.props.onSurveySubmit)}>
+                    {this.renderFields()}
+                    <Link className="red btn-flat left white-text" to="/surveys">Cancel</Link>
+					<button className="teal btn-flat right white-text" type="submit">
+						Next
+						<i className="material-icons right">done</i>
+					</button>
+				</form>
+			</div>
+		);
+	}
+}
+
+
+
+function validate(values) {
+    const errors = {};
+
+    errors.recipients = validateEmails(values.recipients || "");
+
+    _.each(formFields, ({ name, label }) => {
+        if (!values[name]) {
+            errors[name] = 'You must enter a value for the ' + [label]; 
+        }
+    });
+
+    return errors;
+}
+
+export default reduxForm({
+    validate,
+    form: 'surveyForm',
+    destroyOnUnmount: false
+})(SurveyForm);
--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const cookieSession = require('cookie-session');
 const passport = require('passport');
 const bodyParser = require('body-parser');
 require('./models/User');
+require('./models/Survey');
 require('./services/passport');
+
 
 
 monngoose.connect(keys.mongoURI);
@@ -25,6 +27,7 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
+require("./routes/surveyRoutes")(app);
 
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static('client/build'));
