ee07f40cb479b07cfa446c705793b0e6c4b89376
avaughn32 <47760804+avaughn32@users.noreply.github.com>
2019-09-19 09:38:06 -0700

    Update AboutUs.js

--- client/src/components/landing/AboutUs.js
+++ client/src/components/landing/AboutUs.js
@@ -9,6 +9,8 @@ class AboutUs extends Component {
 						<div className="container">
 							<h3>About Us</h3>
 							<p>We are a growing Heating and Air Company located in El Dorado Hills, CA.</p>
+							<h5>Contractor's License</h5>
+							<p>Held by Aaron Vaughn</p>
 						</div>
 					</section>
 				</div>
