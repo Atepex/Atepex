3a91bda76c26e341264278cf2b979d01c7f72539
Tyrus Nanato Tritt <32111332+TyrusTritt@users.noreply.github.com>
2019-09-24 09:32:18 -0700

    Update Services.js

--- client/src/components/landing/Services.js
+++ client/src/components/landing/Services.js
@@ -1,26 +1,48 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
-const serv = [
-	{name: 'Repair & Replacement'},
-	{name: 'Text 2'},
-	{name: 'Text 3'}
-]
 
-const miniServ = [
-	{item: 'Text 1'},
-	{item: 'Text 2'},
-	{item: 'Text 3'}
+const reServ = [
+	{item: 'AC Repair'},
+	{item: 'Ductwork Replacement'},
+	{item: 'Furnace Repair'},
+	{item: 'Heat Pump Repair'},
+	{item: 'Water Heater Replacement'}
+]
+const maServ = [
+	{item: 'AC Maintanance'},
+	{item: 'Maintanance Contracts'},
+	{item: 'Furnace Maintanance'},
+	{item: 'Heat Pump Maintanance'}
+]
+const inServ = [
+	{item: 'AC Installation'},
+	{item: 'Furnance Installation'},
+	{item: 'Heat Pump Installation'},
+	{item: 'Thermostat Installation'}
 ]
 
 class Services extends Component {
-	renderContent()
+	renderContentRe()
 	{
-		return _.map(serv, ({name}) => {
+		return _.map(reServ, ({item}) => {
 			return (
-				<h3>{name}</h3>
+				<li>{item}</li>	
 			);
+		});
+	}
+	renderContentMa()
+	{
+		return _.map(maServ, ({item}) => {
 			return (
-				<li>{miniServ[serv]}</li>
+				<li>{item}</li>	
+			);
+		});
+	}
+	renderContentIn()
+	{
+		return _.map(inServ, ({item}) => {
+			return (
+				<li>{item}</li>	
 			);
 		});
 	}
@@ -31,7 +53,21 @@ class Services extends Component {
 					<section>
 						<div className="container">
 							<h2>Services</h2>
-							<ul>{this.renderContent()}</ul>
+							<p></p>
+							<div>
+								<h3>Repair & Replacement</h3>
+								<l>{this.renderContentRe()}</l>
+							</div>
+							<p></p>
+							<div>
+								<h3>Maintenance</h3>
+								<l>{this.renderContentMa()}</l>
+							</div>
+							<p></p>
+							<div>
+								<h3>Installation</h3>
+								<l>{this.renderContentIn()}</l>
+							</div>
 						</div>
 					</section>
 				</div>
