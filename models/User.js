9cad41f915f28b69bd590b62dda95ba225e84b91
ehaddy <e_haddy@hotmail.com>
2019-10-14 20:04:07 -0700

    admin edit user

--- models/User.js
+++ models/User.js
@@ -6,6 +6,8 @@ const userSchema = new Schema ({
     firstName: String,
     lastName: String,
     email: String,
+    phone: String,
+    zip: String,
     admin: { type: Boolean, default: false }
 });
 
--- routes/clientRoutes.js
+++ routes/clientRoutes.js
@@ -11,4 +11,33 @@ module.exports = app => {
     
         res.status(404).send('Not Found');
       });
+
+      
+      app.post("/api/modifyclient", async(req, res) => {
+        const { _id, fname, lname, phone, zip, email } = req.body;
+        User.findOne({_id: _id}, function(err, user) {
+          if (!err) {
+            user.firstName = fname;
+            user.lastName = lname;
+            user.email = email;
+            user.phone = phone;
+            user.zip = zip;
+            user.save();
+            return res.status(200);
+          }
+        })
+        res.status(404);
+      });
+
+      app.post("/api/deleteclient", async(req, res) => {
+        const { _id } = req.body
+        User.remove({_id: _id}, function(err) {
+          if (!err) {
+            return res.status(200);
+          }
+
+          return res.status(404);
+        })
+      });
+
 };
